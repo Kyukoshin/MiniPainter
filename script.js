be262ef27d048d63ec68467f122cdff70cd3bd62
RemiBertrand <rbertrand21@gmail.com>
2023-02-18 00:29:52 +0100

    Minor changes

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-let colori;
+let colorArmor;
 const defaultColor = "#BD0A0A";
 
 window.addEventListener("load", startup, false);
 
 function startup() {
-    colori = document.querySelector("#armorColor");
-    colori.value = defaultColor;
-    colori.addEventListener("input", updateFirst, false);
-    colori.addEventListener("change", updateAll, false);
+    colorArmor = document.querySelector("#armorColor");
+    colorArmor.value = defaultColor;
+    colorArmor.addEventListener("input", updateFirst, false);
+    colorArmor.addEventListener("change", updateAll, false);
 }
 
 function updateFirst(event) {
     const p = document.querySelector(".armor");
     if (p) {
         console.log("yes");
-        p.style.filter = "opacity(0.3) drop-shadow(0 0 0 "+event.target.value+")";
+        p.style.filter = "opacity(.3) drop-shadow(0 0 0 "+event.target.value+")";
     }
 }
 
